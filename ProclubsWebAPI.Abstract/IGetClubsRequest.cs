642bed4c03b0ed2ab5362d49775ca3b0fb7a6ade
PlayoffError <73446700+PlayoffError@users.noreply.github.com>
2023-03-29 18:33:52 -0400

    Added web request interfaces and gitignore file

---
+++ ProclubsWebAPI.Abstract/IGetClubsRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProclubsWebAPI.Abstract
+{
+    /// <summary>
+    /// Represents a web request to get information about clubs
+    /// </summary>
+    public interface IGetClubsRequest
+    {
+        /// <summary>
+        /// The console platform to search on.
+        /// 
+        /// Valid values are:
+        /// ps5 
+        /// ps4 
+        /// xbox-series-xs 
+        /// xboxone
+        /// </summary>
+        public string Platform { get; }
+
+        /// <summary>
+        /// The name or partial name of the club to search for.
+        /// </summary>
+        public string SearchName { get; }
+
+        /// <summary>
+        /// Call to get the information for the club(s) that match the search criteria.
+        /// </summary>
+        /// <returns>JSON string containing the club information.</returns>
+        public Task<string> GetClubs();
+    }
+}
---
+++ ProclubsWebAPI.Abstract/IGetGamesRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProclubsWebAPI.Abstract
+{
+    public interface IGetGamesRequest
+    {
+        /// <summary>
+        /// Numeric ID of the club to find games for
+        /// </summary>
+        public long ClubID { get; }
+
+        /// <summary>
+        /// Match type of games
+        /// 
+        /// Valid values are:
+        /// gameType5, 
+        /// gameType10, 
+        /// club_private
+        /// 
+        /// </summary>
+        public string MatchType { get; }
+
+        /// <summary>
+        /// Call to get the information for the games that match the search criteria.
+        /// </summary>
+        /// <returns>JSON string containing the game information.</returns>
+        public Task<string> GetGames();
+    }    
+}
